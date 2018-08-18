18c17e50acab3bf4faa33ba242c30c9a14688330
Jeevanjot <jeevanjotsingh@yandex.com>
2018-08-18 21:47:44 +0530

    Un follow on single user working
    un following single user works.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,14 @@
-$('document').ready(function() {
+var signedin;
+$(document).ready(function() {
+  $('.modal').modal();
+});
+
+$(document).ready(function() {
+  if (localStorage.getItem("voter") === null || localStorage.getItem("pass") === null || localStorage.getItem("voter") === "" || localStorage.getItem("pass") === "") {
+    signedin = false;
+  } else {
+    signedin = true;
+  }
 
 });
 
@@ -10,37 +20,59 @@ $('#searchbtn').click(function() {
     })
   } else {
     steem.api.getFollowing(searchname, 0, null, 1000, function(err, result) {
+      $('.collection').empty();
       for (var i = 0; i < result.length; i++) {
-        $('.collection').append("<li class='collection-item'> <div><a href='https://steemit.com/@" + result[i].following + "'>" + result[i].following + "</a><a href='#!' class='secondary-content' onClick='unfollow(\""+ result[i].following+ "\")'><i class='material-icons'>clear</i></a></div> </li>");
+        $('.collection').append("<li class='collection-item'> <div><a href='https://steemit.com/@" + result[i].following + "'>" + result[i].following + "</a><a href='#!' class='secondary-content' onClick='unfollow(\"" + result[i].following + "\")'><i class='material-icons'>clear</i></a></div> </li>");
       }
     });
   }
 });
 
-function unfollow(name)
-{
+
+function unfollow(name) {
   //Your JavaScript Code Here...
-  
-  var follower = "genievot"; // Your username
-  var following = 'steemjs'; // User to follow
-  var json = JSON.stringify(
-    ['follow', {
-      follower: follower,
-      following: following,
-      what: []
-    }]
-  );
-  var postingWif = "5JMSgaec4GEUpMVebrE9EtNRsgPQ8Yqi3XeVEMvjGUGUeQNKRgh"
+  if (localStorage.getItem("voter") === null || localStorage.getItem("pass") === null || localStorage.getItem("voter") === "" || localStorage.getItem("pass") === "") {
+    $('.modal').modal('open');
+  } else {
+    var follower = localStorage.getItem("voter"); // Your username
+    var following = name; // User to follow
+    var json = JSON.stringify(
+      ['follow', {
+        follower: follower,
+        following: following,
+        what: []
+      }]
+    );
+    var postingWif = steem.auth.toWif(localStorage.getItem("voter"), localStorage.getItem("pass"), 'posting');
 
     steem.broadcast.customJson(
-    postingWif,
-    [], // Required_auths
-    [follower], // Required Posting Auths
-    'follow', // Id
-    json, //
-    function(err, result) {
-      console.log(err, result);
-    }
-  );
-  console.log(name);
+      postingWif, [], // Required_auths
+      [follower], // Required Posting Auths
+      'follow', // Id
+      json, //
+      function(err, result) {
+        console.log(err, result);
+        if (err) {
+          M.toast({
+            html: 'Error! Make sure you entered right username and passowrd and retry again.'
+          })
+        }
+        if (!err) {
+          M.toast({
+            html: 'Unfollowed!- ' + name
+          })
+        }
+      }
+    );
+  }
+
+}
+
+function sign_show() {
+  $('.modal').modal('open');
+}
+
+function sign_in() {
+  localStorage.setItem("voter", $('#user_name').val());
+  localStorage.setItem("pass", $('#password').val());
 }
