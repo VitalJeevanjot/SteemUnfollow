bba49a3ea791b322606f227b01c88af575084c4f
Jeevanjot <jeevanjotsingh@yandex.com>
2018-08-18 21:59:42 +0530

    adding new features
    new error protection
    - search = voter
    - must signin
    - local storage

--- js/index.js
+++ js/index.js
@@ -11,28 +11,38 @@ $(document).ready(function() {
   }
 
 });
-
+var searchname;
 $('#searchbtn').click(function() {
-  var searchname = $('#search_name').val();
+  searchname = $('#search_name').val();
+  localStorage.setItem('searchName', searchname);
   if (searchname === null || searchname === "") {
     M.toast({
       html: 'Enter a name!'
     })
   } else {
-    steem.api.getFollowing(searchname, 0, null, 1000, function(err, result) {
-      $('.collection').empty();
-      for (var i = 0; i < result.length; i++) {
-        $('.collection').append("<li class='collection-item'> <div><a href='https://steemit.com/@" + result[i].following + "'>" + result[i].following + "</a><a href='#!' class='secondary-content' onClick='unfollow(\"" + result[i].following + "\")'><i class='material-icons'>clear</i></a></div> </li>");
-      }
-    });
+    listFollowing();
   }
 });
+
+
+function listFollowing() {
+  steem.api.getFollowing(searchname, 0, null, 1000, function(err, result) {
+    $('.collection').empty();
+    for (var i = 0; i < result.length; i++) {
+      $('.collection').append("<li class='collection-item'> <div><a href='https://steemit.com/@" + result[i].following + "'>" + result[i].following + "</a><a href='#!' class='secondary-content' onClick='unfollow(\"" + result[i].following + "\")'><i class='material-icons'>clear</i></a></div> </li>");
+    }
+  });
+}
 
 
 function unfollow(name) {
   //Your JavaScript Code Here...
   if (localStorage.getItem("voter") === null || localStorage.getItem("pass") === null || localStorage.getItem("voter") === "" || localStorage.getItem("pass") === "") {
     $('.modal').modal('open');
+  } else if (localStorage.getItem("searchName") !== localStorage.getItem("voter")) {
+    M.toast({
+      html: 'Enter the right value in search box and again hit search'
+    })
   } else {
     var follower = localStorage.getItem("voter"); // Your username
     var following = name; // User to follow
@@ -61,6 +71,7 @@ function unfollow(name) {
           M.toast({
             html: 'Unfollowed!- ' + name
           })
+          listFollowing();
         }
       }
     );
